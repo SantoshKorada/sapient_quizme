5edbb4d3af65ea4aa469dd6bcc7341000d607eca
Korada, Santosh <KoradaS@DNB.com>
2022-07-09 14:01:56 +0530

    changes

--- src/main/java/com/sapient/quizme/SapientQuizmeApplication.java
+++ src/main/java/com/sapient/quizme/SapientQuizmeApplication.java
@@ -2,21 +2,28 @@ package com.sapient.quizme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SapientQuizmeApplication extends SpringBootServletInitializer {
+/*
+ * public class SapientQuizmeApplication extends SpringBootServletInitializer {
+ * 
+ * public static void main(String[] args) {
+ * SpringApplication.run(SapientQuizmeApplication.class, args); }
+ * 
+ * @Override protected SpringApplicationBuilder
+ * configure(SpringApplicationBuilder builder) { return
+ * builder.sources(SapientQuizmeApplication.class); }
+ * 
+ * @Bean public RestTemplate restTemplate() { return new RestTemplate(); }
+ * 
+ * }
+ */
+public class SapientQuizmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SapientQuizmeApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SapientQuizmeApplication.class);
 	}
 
 	@Bean
